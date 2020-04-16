06e4c304be3e52e89f87a80e1489202f97c4e999
Michael Gunn <gunny1606@gmail.com>
2020-04-16 13:35:53 +1000

    c2

--- GHtest1.py
+++ GHtest1.py
@@ -1,6 +1,12 @@
+# example of a class setup and initialisation
 class TestClass:
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    def test_function(self, x):
-        return x**2
+    def test_function(self):
+        return self.x*2
+
+tc = TestClass(4,3)
+
+print(tc.test_function())
